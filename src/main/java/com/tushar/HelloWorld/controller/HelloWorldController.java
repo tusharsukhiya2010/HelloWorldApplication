0ee1c7059d1c65d59cea60f674bcde2527f7b900
tusharsukhiya2010 <tusharsukhiya2010@gmail.com>
2018-10-18 13:03:50 +0530

    Hello World Spring Boot Application

---
+++ src/main/java/com/tushar/HelloWorld/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.tushar.HelloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/sayHello")
+	public String sayHelloWorld() {
+		return "Hello World";
+	}
+}
